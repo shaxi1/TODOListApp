9d131e0353492e9734f0ec97505291d266392a30
Marek Szulc <235991@edu.p.lodz.pl>
2023-05-25 22:10:20 +0200

    Add creation and connect to sqlite with DatabaseHelper

--- app/src/main/java/com/todo/todolistapp/DatabaseHelper.java
+++ app/src/main/java/com/todo/todolistapp/DatabaseHelper.java
@@ -1,12 +1,38 @@
 package com.todo.todolistapp;
 
-// connect to the sqlite, crud tasks, crud settings
-public class DatabaseHelper {
-    public DatabaseHelper() {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+// connect to the sqlite, crud tasks, crud settings
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "todoapp.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // create tasks table
+        db.execSQL("CREATE TABLE tasks (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, creation_date INTEGER, due_date INTEGER, is_completed INTEGER, notifications_enabled INTEGER, category TEXT)");
 
+        // create settings table
+        db.execSQL("CREATE TABLE settings (_id INTEGER PRIMARY KEY AUTOINCREMENT, hide_finished_tasks INTEGER, filter_category TEXT, notify_before INTEGER)");
 
+        /* set default values */
+
+        db.execSQL("INSERT INTO settings (name, value) VALUES ('hide_finished_tasks', 'false')");
+        /* "All" means don't filter at all */
+        db.execSQL("INSERT INTO settings (name, value) VALUES ('filter_category', 'All')");
+        /* notification X minutes before due time */
+        db.execSQL("INSERT INTO settings (name, value) VALUES ('notify_before', '60')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO: migrate data
+    }
 }
