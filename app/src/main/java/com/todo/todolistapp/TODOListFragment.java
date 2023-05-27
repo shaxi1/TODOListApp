89d3463a64baf968415c50b70c16334c4f036812
Marek Szulc <235991@edu.p.lodz.pl>
2023-05-27 18:34:23 +0200

    Implement task filtering of the tasks list in TODOListFragment

--- app/src/main/java/com/todo/todolistapp/TODOListFragment.java
+++ app/src/main/java/com/todo/todolistapp/TODOListFragment.java
@@ -7,14 +7,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TODOListFragment extends Fragment {
+    private DatabaseHelper databaseHelper;
+    private TaskAdapter taskAdapter;
+    private RecyclerView recyclerView;
     public TODOListFragment() {
         // Required empty public constructor
     }
@@ -26,6 +36,7 @@ public class TODOListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        databaseHelper = new DatabaseHelper(getActivity());
     }
 
     @Override
@@ -34,9 +45,41 @@ public class TODOListFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_todo_list, container, false);
 
+        taskAdapter = new TaskAdapter(getActivity());
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(taskAdapter);
+
         configureFloatingButtonClick(view);
+        configureSpinner(view);
 
         return view;
+    }
+
+    private void configureSpinner(View view) {
+        Spinner spinner = view.findViewById(R.id.category_spinner);
+        if (spinner == null) {
+            return;
+        }
+
+        List<String> categories = new ArrayList<>();
+        categories.add("All");
+        categories.addAll(databaseHelper.getCategories());
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(view.getContext(), android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0, false);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                taskAdapter.setFilterCategory(categories.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // do nothing
+            }
+        });
     }
 
     private void configureFloatingButtonClick(View view) {
