6975358dd854fcdd3fa7e53bc8ddc83916c63c9f
Marek Szulc <235991@edu.p.lodz.pl>
2023-05-27 18:24:24 +0200

    Remove unused import

--- app/src/main/java/com/todo/todolistapp/TaskAdapter.java
+++ app/src/main/java/com/todo/todolistapp/TaskAdapter.java
@@ -2,7 +2,6 @@ package com.todo.todolistapp;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
